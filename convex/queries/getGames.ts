8c960a46365339803816cb711c455f34c83f8099
Fernando Emir <fernando.faour@davinci.edu.ar>
2025-11-14 15:10:01 -0300

    pre deploy 3
    Se prueba separar la carpeta del backend del deploy del front

--- convex/queries/getGames.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "../_generated/server";
-import type { Doc } from "../_generated/dataModel";
-
-export const getGames = query({
-  args: {},
-  handler: async (ctx): Promise<Doc<"games">[]> => {
-    return await ctx.db.query("games").collect();
-  },
-});
